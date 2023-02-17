a63ae7d737fc030f1c59e71bfccf5aec7b092e32
InvertedOwl <70726294+InvertedOwl@users.noreply.github.com>
2023-02-17 15:44:38 -0700

    dfs

--- src/PIDMotor.cpp
+++ src/PIDMotor.cpp
@@ -2,33 +2,6 @@
 #include <sys/types.h>
 
 void PIDMotor::tick() {
-    // const float calculatedRPM = this->flywheelSpeed->get_velocity() / 6.0f; // Degrees per second to RPM
-    // const float calculatedTarget = (target/127.0f) * 3000; // RPM
-
-    // const double kP = 0.1;
-    // const double kI = 0.05;
-    // const double kD = 0.01;
-
-    // // Measure the current speed of the flywheel motor
-    // double currentSpeed = calculatedRPM;
-    
-    // // Calculate the error between the current speed and target speed
-    // double error = calculatedTarget - currentSpeed;
-    
-    // // Add the error to the total error (for I term)
-    // totalError += error;
-    
-    // // Calculate the PID output
-    // double output = kP * error + kI * totalError + kD * (error - prevError);
-    
-    // // Save the current error for the next iteration
-    // prevError = error;
-    
-    // Set the motor speed to the PID output
-    std::cout << "PID TICK" << std::endl;
-
-    motor->target = this->target;
-    motor1->target = this->target;
 }
 
 PIDMotor::PIDMotor(MotorH m, MotorH m1)  {
